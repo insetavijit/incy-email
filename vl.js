1315076d3d1a4318d8c3aa52f590cd54a599b5f9
avijit sarkar <insetavijit@gmail.com>
2018-08-01 22:27:45 +0530

    Created Cmnds : + clr : (bin/dist/all )' + clear + to:jade

---
+++ vl.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "src":"src",
+    "dist":"dist",
+    "bin":".bin",
+    "doc":"doc",
+    "readme":'/doc/README/',
+
+    //registerting the tools
+    "tools":{
+        "html2jade":{
+            "src": "doc/TOOLS/HTML2JADE/**.html",
+            "dist":".bin"
+        }
+    }
+}
